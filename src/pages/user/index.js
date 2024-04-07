c59d28b1e9f0357cd30f7867d48a1d3d739677b4
Khoa Minh <khoanguyenminh222@gmail.com>
2024-04-07 22:43:28 +0700

    add schedule

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -7,7 +7,6 @@ function User() {
     const [users, setUsers] = useState([]);
     const [showModal, setShowModal] = useState(false);
     const [selectedUser, setSelectedUser] = useState(null);
-    const [weekSchedules, setWeekSchedules] = useState([]);
 
     const handleAddSchedule = (user) => {
         if (user) {
@@ -32,61 +31,13 @@ function User() {
                 console.error('Error fetching users:', error);
             }
         };
-        const fetchWeekSchedules = async () => {
-            try {
-                const responses = await Promise.all(users.map(user => axios.get(`${baseURL}${weekScheduleRoutes}/${user._id}`)));
-                const schedules = responses.map(response => response.data);
-                setWeekSchedules(schedules);
-            } catch (error) {
-                console.error('Error fetching week schedules:', error);
-            }
-        };
         fetchUsers();
-        fetchWeekSchedules();
-    }, [users]);
+    }, []);
 
     return (
         <div className="container mx-auto max-h-full bg-white p-6">
             <h1 className="text-3xl font-semibold my-4">Ca làm việc của nhân viên 1 tuần</h1>
-            {/* hiển thị lịch làm việc cùng thời gian */}
-            <table className="min-w-full divide-y divide-gray-200">
-                <thead className="bg-gray-50">
-                    <tr>
-                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nhân viên</th>
-                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Thứ 2</th>
-                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Thứ 3</th>
-                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Thứ 4</th>
-                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Thứ 5</th>
-                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Thứ 6</th>
-                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Thứ 7</th>
-                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Chủ nhật</th>
-                    </tr>
-                </thead>
-                <tbody className="bg-white divide-y divide-gray-200">
-                    {users.map((user, userIndex) => (
-                        <tr key={user._id}>
-                            <td className="px-6 py-4 whitespace-nowrap">{user.fullname}</td>
-                            {[0, 1, 2, 3, 4, 5, 6].map(dayIndex => (
-                                <td key={dayIndex} className="px-6 py-4 whitespace-nowrap">
-                                    {weekSchedules[userIndex] && weekSchedules[userIndex].weeks[0][dayIndex].length > 0 ? (
-                                        <div>
-                                            {weekSchedules[userIndex].weeks[0][dayIndex].map(schedule => (
-                                                <div key={schedule._id}>
-                                                    {schedule.startTime} - {schedule.endTime}
-                                                </div>
-                                            ))}
-                                        </div>
-                                    ) : (
-                                        <div>Không có lịch</div>
-                                    )}
-                                </td>
-                            ))}
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-
-            {/* hiên thị danh sách nhân viên*/}
+            {/* hiển thị lịch làm việc cảu nhân viên */}
             <h1 className="text-3xl font-semibold my-4">Danh sách nhân viên</h1>
             <table className="min-w-full divide-y divide-slate-400">
                 <thead className="bg-slate-200">
