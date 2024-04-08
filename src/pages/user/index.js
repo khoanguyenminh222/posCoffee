a62fb29fcb57a27d96b428d88d040e7f3694c7f9
Khoa Minh <khoanguyenminh222@gmail.com>
2024-04-08 09:27:40 +0700

    display schedule

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -7,6 +7,24 @@ function User() {
     const [users, setUsers] = useState([]);
     const [showModal, setShowModal] = useState(false);
     const [selectedUser, setSelectedUser] = useState(null);
+    const [startDate, setStartDate] = useState(null);
+    const [endDate, setEndDate] = useState(null);
+    const [schedule, setSchedule] = useState(null)
+
+    useEffect(() => {
+        const currentDate = new Date();
+        const startOfWeek = new Date(currentDate);
+        startOfWeek.setDate(currentDate.getDate() - currentDate.getDay() + 1); // Set start of week to Monday
+        const endOfWeek = new Date(currentDate);
+        endOfWeek.setDate(currentDate.getDate() - currentDate.getDay() + 7); // Set end of week to Sunday
+        const startDateString = startOfWeek.toISOString().slice(0, 10);
+        const endDateString = endOfWeek.toISOString().slice(0, 10);
+
+        setStartDate(startDateString);
+        setEndDate(endDateString);
+
+        fetchWeekScheduleForAllUsers(startDateString, endDateString);
+    }, []);
 
     const handleAddSchedule = (user) => {
         if (user) {
@@ -22,6 +40,17 @@ function User() {
         setSelectedUser(null);
     };
 
+    const fetchWeekScheduleForAllUsers = async (start, end) => {
+        console.log(start)
+        try {
+            const response = await axios.get(`${baseURL}${weekScheduleRoutes}?startDate=${start}&endDate=${end}`);
+            setSchedule(response.data);
+            console.log("response", response.data)
+        } catch (error) {
+            console.error('Error fetching week schedules for all users:', error);
+        }
+    };
+
     useEffect(() => {
         const fetchUsers = async () => {
             try {
@@ -34,10 +63,78 @@ function User() {
         fetchUsers();
     }, []);
 
+
     return (
         <div className="container mx-auto max-h-full bg-white p-6">
             <h1 className="text-3xl font-semibold my-4">Ca làm việc của nhân viên 1 tuần</h1>
-            {/* hiển thị lịch làm việc cảu nhân viên */}
+            <div>
+                <label htmlFor="start-date">Start Date:</label>
+                <input type="date" id="start-date" value={startDate || ''} onChange={(e) => setStartDate(e.target.value)} />
+            </div>
+            <div>
+                <label htmlFor="end-date">End Date:</label>
+                <input type="date" id="end-date" value={endDate || ''} onChange={(e) => setEndDate(e.target.value)} />
+            </div>
+            {schedule && startDate && endDate && (
+                <div>
+                    <h2 className="text-lg font-semibold">Lịch làm việc của nhân viên</h2>
+                    <table className="min-w-full divide-y divide-slate-400">
+                        <thead className="bg-slate-200">
+                            <tr>
+                                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nhân viên</th>
+                                {['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'].map((day, index) => (
+                                    <th key={index} scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{day}</th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody className="bg-white divide-y divide-gray-200">
+                            {users.map(user => {
+                                const userSchedule = schedule.find(item => item.user._id === user._id);
+                                if (!userSchedule) return null; // Không có lịch làm việc cho người dùng này
+
+                                return (
+                                    <React.Fragment key={user._id}>
+                                        {console.log("schedule", schedule)}
+                                        {userSchedule.weeks.map((week, weekIndex) => {
+                                            const weekStartDate = new Date(week.startDate);
+                                            const weekEndDate = new Date(week.endDate);
+
+                                            // Kiểm tra xem tuần này có trong khoảng start date và end date không
+                                            if (weekEndDate >= new Date(startDate) && weekStartDate <= new Date(endDate)) {
+                                                return (
+                                                    <tr key={`${user._id}_${weekIndex}`}>
+                                                        <td className="px-6 py-4 whitespace-nowrap">{user.fullname}</td>
+                                                        {['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'].map((day, dayIndex) => {
+                                                            const dayShifts = week[day.toLowerCase()] || [];
+                                                            return (
+                                                                <td key={dayIndex} className="px-6 py-4 whitespace-nowrap">
+                                                                    {dayShifts.length > 0 ? (
+                                                                        <ul className="list-disc">
+                                                                            {dayShifts.map((shift, shiftIndex) => (
+                                                                                <li key={shiftIndex}>{shift.startTime} - {shift.endTime}</li>
+                                                                            ))}
+                                                                        </ul>
+                                                                    ) : (
+                                                                        <span>Không có ca làm việc</span>
+                                                                    )}
+                                                                </td>
+                                                            );
+                                                        })}
+                                                    </tr>
+                                                );
+                                            } else {
+                                                return null;
+                                            }
+                                        })}
+                                    </React.Fragment>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+            )}
+
+            {/* hiển thị danh sách nhân viên */}
             <h1 className="text-3xl font-semibold my-4">Danh sách nhân viên</h1>
             <table className="min-w-full divide-y divide-slate-400">
                 <thead className="bg-slate-200">
